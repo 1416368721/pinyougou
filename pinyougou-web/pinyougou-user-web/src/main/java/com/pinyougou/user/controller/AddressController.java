9be3ece91d1956322b57231f3663ea89a8086adb
demonbackfire <932980738@qq.com>
2019-03-27 11:16:16 +0800

    查找地址

---
+++ pinyougou-web/pinyougou-user-web/src/main/java/com/pinyougou/user/controller/AddressController.java
@@ -0,0 +1,23 @@
+package com.pinyougou.user.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.Address;
+import com.pinyougou.service.AddressService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+    @Reference(timeout = 10000)
+    private AddressService addressService;
+    @GetMapping("/findAll")
+    public List<Address> findAll(){
+
+        return addressService.findAll();
+    }
+}
