60088d24b75562dd115623f6ac18d355e0202c0f
dzk <1057426326@qq.com>
2019-03-27 17:28:46 +0800

    Merge branch 'master' of https://github.com/1416368721/pinyougou
    # Conflicts:
    #	pinyougou-web/pinyougou-shop-web/src/main/webapp/admin/index.html
    #	pinyougou-web/pinyougou-shop-web/src/main/webapp/js/controller/indexController.js
    修改密码模块完成

--- pinyougou-web/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou-web/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -4,9 +4,11 @@ app.controller('cartController', function ($scope, $controller, baseService) {
     $controller('baseController', {$scope:$scope});
     $scope.sellerName=[];
     $scope.ids=[];
+    $scope.xMids=[];
+    $scope.aDids=[];
     $scope.checkedArr=[];
     $scope.checkedArr2=[];
-    $scope.ckAll=false;
+    $scope.check2=false;
     $scope.findIdsCart=function () {
         baseService.sendGet("/cart/findIdsCart")
             .then(function (response) {
@@ -29,76 +31,114 @@ app.controller('cartController', function ($scope, $controller, baseService) {
                 }
             });
     };
-    //商家处选择框
-    $scope.updateSelection=function ($event,id,i) {
-        if($event.target.checked){
-            $scope.sellerName.push(id);
-        }else{
-            var idx=$scope.sellerName.indexOf(id);
-            $scope.sellerName.splice(idx,1);
-        }
-        // alert($scope.ids);
-        $scope.checkedArr[i]=$event.target.checked;
-        $scope.ckAll=$scope.carts.length==$scope.sellerName.length;
-        // alert($scope.sellerName);
-    };
-    //最大的全选
-    $scope.checkAll=function ($event) {
-        $scope.sellerName=[];
-        // alert($scope.carts);
-        for(var i=0;i<$scope.carts.length;i++){
-            $scope.checkedArr[i]=$event.target.checked;
-            if($event.target.checked){
-                $scope.sellerName.push($scope.carts[i].sellerName);
-                // var orderItems=$scope.carts[i];
-                // for(var j=0;j<orderItems.length;j++){
-                //     $scope.ids.push(orderItems[j].itemId)
-                // }
-                // $scope.checkedArr=
-            }
-        }
 
-        $scope.ckAll=$scope.carts.length==$scope.ids.length;
-    };
-    //商品选择
-    $scope.updateSelection2=function ($event,id,i,sellerName) {
+    // 商品选择
+    $scope.updateSelection2=function ($event,id,i,cart) {
         // alert(sellerName);
+        var sellerName=cart.sellerName;
+        // alert(sellerName);
+        // alert(cart.orderItems.length);
         $scope.isSelectNum=0;
         $scope.isSelectMoney=0;
-        if ($event.target.checked) {
-            $scope.ids.push(id);
-        } else {
-            var idx = $scope.ids.indexOf(id);
-            $scope.ids.splice(idx, 1);
-        }
-        // alert($scope.ids);
-        for (var b = 0; $scope.carts.length > b;b++) {
-            var orderItems = $scope.carts[b].orderItems;
-            // alert(orderItems);
-            for (var c = 0; orderItems.length > c; c++) {
-                var orderItem=orderItems[c];
-                // alert("遍历："+orderItem.itemId);
-                for (var a = 0; $scope.ids.length > a; a++) {
-                    // alert($scope.ids[a]);
-                    if (orderItem.itemId == $scope.ids[a]){
-                        // alert(orderItems[c].itemId);
-                        $scope.isSelectNum += orderItems[c].num;
-                        $scope.isSelectMoney += orderItems[c].totalFee;
+
+        if("小米"==sellerName) {
+            // alert("小米");
+            if ($event.target.checked) {
+                $scope.xMids.push(id);
+            } else {
+                var idx = $scope.xMids.indexOf(id);
+                $scope.xMids.splice(idx, 1);
+            }
+            for (var b = 0; $scope.carts.length > b; b++) {
+                var orderItems = $scope.carts[b].orderItems;
+                // alert(orderItems);
+                for (var c = 0; orderItems.length > c; c++) {
+                    var orderItem = orderItems[c];
+                    // alert("遍历："+orderItem.itemId);
+
+                    for (var a = 0; $scope.xMids.length > a; a++) {
+                        // alert($scope.ids[a]);
+                        if (orderItem.itemId == $scope.xMids[a]) {
+                            // alert(orderItems[c].itemId);
+                            $scope.isSelectNum += orderItems[c].num;
+                            $scope.isSelectMoney += orderItems[c].totalFee;
+
+                        }
+                    }
+                }
+            }
+            // alert(cart.orderItems.length);
+            $scope.ckAll=cart.orderItems.length==$scope.xMids.length;
+            // alert("小米:"+$scope.xMids);
+        }else if("品优购"==sellerName){
+            if ($event.target.checked) {
+                $scope.aDids.push(id);
+            } else {
+                var idx = $scope.aDids.indexOf(id);
+                $scope.aDids.splice(idx, 1);
+            }
+            for (var b = 0; $scope.carts.length > b; b++) {
+                var orderItems = $scope.carts[b].orderItems;
+                // alert(orderItems);
+                for (var c = 0; orderItems.length > c; c++) {
+                    var orderItem = orderItems[c];
+                    // alert("遍历："+orderItem.itemId);
+                    for (var a = 0; $scope.aDids.length > a; a++) {
+                        // alert($scope.ids[a]);
+                        if (orderItem.itemId == $scope.aDids[a]) {
+                            // alert(orderItems[c].itemId);
+                            $scope.isSelectNum += orderItems[c].num;
+                            $scope.isSelectMoney += orderItems[c].totalFee;
+                        }
                     }
                 }
             }
         }
-    };
-        // alert($scope.ids);
-        // $scope.checkedArr[i]=$event.target.checked;
-        // $scope.ckAll=$scope.carts.length==$scope.sellerName.length;
-        // alert($scope.sellerName);
-    //    通过商家名来获取
-    //     for(var i=0;$scope.carts.length>i;i++){
-    //         if($scope.carts[i].sellerName==sellerName){
-    //         // alert($scope.carts[i].sellerName);
-    //             $scope.checkedArr[i]=$event.target.checked;
+       $scope.ckAll=cart.orderItems.length==$scope.aDids.length
+           // alert($scope.ckAll);
 
+        // alert("品优购:"+$scope.aDids);
+    };
+
+    $scope.checkAll = function($event,cart){
+        if(cart.sellerName=="品优购") {
+            // 清空用户选择的ids
+            $scope.aDids = [];
+            var orderItem = cart.orderItems;
+            // alert(orderItem.length);
+            // 循环当前页数据数组
+            for (var i = 0; i < orderItem.length; i++) {
+                alert(i);
+                // 初始化数组
+                $scope.checkedArr[cart.sellerName+i] = $event.target.checked;
+                // 判断是否选中
+                if ($event.target.checked) {
+                    // {id}
+                    $scope.aDids.push(orderItem[i].id);
+                }
+            }
+            // 重新赋值，再次绑定checkbox
+            $scope.ckAll = $scope.orderItem.length == $scope.xMids.length;
+        }else if(cart.sellerName=="小米"){
+            // 清空用户选择的ids
+            $scope.xMids = [];
+            var orderItem = cart.orderItems;
+            alert(orderItem.length);
+            // 循环当前页数据数组
+            for (var i = 0; i < orderItem.length; i++) {
+                alert(i);
+                // 初始化数组
+                $scope.checkedArr[cart.sellerName+i] = $event.target.checked;
+                // 判断是否选中
+                if ($event.target.checked) {
+                    // {id}
+                    $scope.xMids.push(orderItem[i].id);
+                }
+            }
+            // 重新赋值，再次绑定checkbox
+            $scope.ckAll = $scope.orderItem.length == $scope.xMids.length;
+        }
+    };
 
 
     //更新页面上选择商品数量和金额
@@ -161,9 +201,9 @@ app.controller('cartController', function ($scope, $controller, baseService) {
                     alert("请先勾选商品");
                 }
             }
-
-
-
     }
+
+
+    //
 
 });
